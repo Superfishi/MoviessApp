dabf7ec19f59abfb988eed954ef6070f8d7d673e
arjan <arjan@DESKTOP-L1L391F>
2017-02-14 15:28:40 +0100

    Add project files.

---
+++ NetflixRouletteApp/NetflixRouletteApp.Droid/MainActivity.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using System.Net;
+
+namespace NetflixRouletteApp.Droid
+{
+    [Activity(Label = "NetflixRouletteApp", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(bundle);
+
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+
+            //if (Build.Brand.Equals("GENERIC", StringComparison.InvariantCultureIgnoreCase))
+            //{
+            //ServicePointManager.ServerCertificateValidationCallback +=
+            //    (sender, cert, chain, sslPolicyErrors) => true;
+            //}
+
+            LoadApplication(new App());
+        }
+    }
+}
+
---
+++ NetflixRouletteApp/NetflixRouletteApp/Models/Movie.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetflixRouletteApp.Models
+{
+    public class Movie
+    {
+        [JsonProperty("show_title")]
+        public string Title { get; set; }
+
+        [JsonProperty("release_year")]
+        public string ReleaseYear { get; set; }
+
+        [JsonProperty("rating")]
+        public string Rating { get; set; }
+
+        [JsonProperty("poster")]
+        public string Poster { get; set; }
+
+        [JsonProperty("summary")]
+        public string Summary { get; set; }
+        
+    }
+}
